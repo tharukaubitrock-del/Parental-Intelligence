e36aa423eb6a19deed665004c22958f2aeee118f
tharukaubitrock-del <tharukau.bitrock@gmail.com>
2025-08-09 23:41:43 +0530

    Add files via upload

--- netlify/functions/subscribe.js
+++ netlify/functions/subscribe.js
@@ -73,16 +73,15 @@ exports.handler = async (event) => {
     statusCode: 200,
     headers: { 'Content-Type': 'text/html' },
     body: `<!DOCTYPE html><html><body>
-      <form id="payhere" method="POST" action="https://sandbox.payhere.lk/pay/o8b4bd664">
+      <!-- in subscribe.js body (temporary, one-time test) -->
+      <form id="payhere" method="POST" action="https://sandbox.payhere.lk/pay/checkout">
         <input type="hidden" name="merchant_id" value="${MERCHANT_ID}">
         <input type="hidden" name="return_url"  value="${returnUrl}">
         <input type="hidden" name="cancel_url"  value="${cancelUrl}">
         <input type="hidden" name="notify_url"  value="${notifyUrl}">
         <input type="hidden" name="order_id"    value="${orderId}">
-        <input type="hidden" name="items"       value="Monthly Subscription">
-        <input type="hidden" name="currency"    value="${currency}">
-        <input type="hidden" name="recurrence"  value="${recurrence}">
-        <input type="hidden" name="duration"    value="${duration}">
+        <input type="hidden" name="items"       value="Test Charge">
+        <input type="hidden" name="currency"    value="LKR">
         <input type="hidden" name="amount"      value="${amount}">
         <input type="hidden" name="first_name"  value="${firstName}">
         <input type="hidden" name="last_name"   value="${lastName}">
@@ -91,6 +90,7 @@ exports.handler = async (event) => {
         <input type="hidden" name="address"     value="${address}">
         <input type="hidden" name="city"        value="${city}">
         <input type="hidden" name="country"     value="${country}">
+        <input type="hidden" name="custom_1"    value="${userId}"> <!-- fallback link -->
         <input type="hidden" name="hash"        value="${hash}">
       </form>
       <script>document.getElementById('payhere').submit();</script>
